dc1f3a2ccf3d8f4c00e3464b23ae13f69784ad0d
Victor <victormilen12@gmail.com>
2024-09-04 16:31:58 -0300

    Lista em ordem crescente

---
+++ questao6.py
@@ -0,0 +1,13 @@
+# Solicita os três valores
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+
+# Coloca os valores em uma lista
+valores = [A, B, C]
+
+# Ordena a lista
+valores.sort()
+
+# Exibe os valores ordenados
+print("Valores em ordem ascendente:", valores)
