cab7ab93fca277f11f31393f8698cfd8e16f5151
Victor Milen <victormilen12@gmail.com>
2024-09-11 16:32:47 -0300

    Recebe uma lista de 8 números que exibe a quantidade de números negativos e a soma dos números positivos.

---
+++ Atividades-ExtraPara/atv2.py
@@ -0,0 +1,15 @@
+numeros = [0] * 8
+quantidade_negativos = 0
+soma_positivos = 0
+
+for n in range(8):
+    numero = float(input(f"Digite o {n+1}º número: "))
+    numeros[n] = numero
+    
+    if numero < 0:
+        quantidade_negativos += 1
+    elif numero > 0:
+        soma_positivos += numero
+
+print(f"Quantidade de números negativos: {quantidade_negativos}")
+print(f"Soma dos números positivos: {soma_positivos:.2f}")
