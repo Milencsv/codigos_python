d9758a4ca6dc94d05442af44d2c10d42e3166000
Victor Milen <victormilen12@gmail.com>
2024-09-11 16:54:17 -0300

    O usuário irá digitar dois valores inteiros positivos onde será ditos quantos deles são multiplos simultaneos de 7 e 11

---
+++ Atividades-ExtraPara/atv7.py
@@ -0,0 +1,8 @@
+A = int(input("Digite um número positivo: "))
+B = int(input("Digite mais um número positivo: "))
+rFinal = 0
+
+for intervalo in range (A ,B +1):
+    if intervalo % 7 == 0 and intervalo % 11 == 0:
+     rFinal = rFinal +1
+print(f"Existe(em) {rFinal} número(os) qua são multiplos simultaneos de 7 e 11")
