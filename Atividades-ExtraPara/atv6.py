b7043eb785f639b9f8e8799fe0c3220e39d28b02
Victor Milen <victormilen12@gmail.com>
2024-09-11 16:53:42 -0300

    O programa irá determinar se o número é perfeito ou não e irá justificar no final

---
+++ Atividades-ExtraPara/atv6.py
@@ -0,0 +1,11 @@
+n = int(input("Digite um número inteiro: "))
+
+soma = 0
+for i in range(1, n):
+    if n % i == 0:
+        soma += i
+
+if soma == n:
+    print(f"{n} é um número perfeito.")
+else:
+    print(f"{n} não é um número perfeito")
