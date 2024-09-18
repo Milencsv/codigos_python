5bf2238890fca31bd495eea201a7c527e6712ef5
Victor Milen <victorilen12@gmail.com>
2024-09-18 15:11:12 -0300

    Como funciona a função no python

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#Função com retorno
+
+def somar(num1, num2):
+    resultado = num1 + num2
+    return resultado #devolvendo o valor para quem chamou a função
+
+print(somar(60 , 9))
+
+#Função sem retorno
+def multiplicar (valor1, valor2):
+    resutlado = valor1 * valor2
+    print(resutlado)
+multiplicar(10,99)
